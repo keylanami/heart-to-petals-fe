07494d22d8623baf82ad9e9444672fa7c7e8dab8
klyroe <keylaanmrr@gmail.com>
2025-12-15 09:11:39 +0700

    add (utils) data dummy

---
+++ app/utils/data.js
@@ -0,0 +1,547 @@
+const bouquet = [
+
+  {
+    name: "Moonlight Serenity",
+    emotion: "peace",
+    flowerset: [{
+      primary: ["White Peony", "Blush Peony"],
+      secondary: ["White Ranunculus", "Blush Ranunculus", "White Hydrangea"],
+      filler: ["White Baby's Breath"],
+      branching: ["White Sweet Pea"]
+    }],
+    amount: [{
+      prim: [3, 2],
+      sec: [4, 3, 1],
+      fill: [2],
+      branch: [3]
+    }]
+  },
+
+  {
+    name: "Crimson Promise",
+    emotion: "peace",
+    flowerset: [{
+      primary: ["Pink Dahlia", "Blush Peony"],
+      secondary: ["Pink Ranunculus", "Blush Garden Rose"],
+      filler: ["Pink Wax Flower"],
+      branching: ["Apple Blossom"]
+    }],
+    amount: [{
+      prim: [3, 2],
+      sec: [3, 2],
+      fill: [2],
+      branch: [1]
+    }]
+  },
+
+  {
+    name: "Sunset Harmony",
+    emotion: "peace",
+    flowerset: [{
+      primary: ["Peach Dahlia", "Peach Peony"],
+      secondary: ["Peach Ranunculus", "Blush Hydrangea"],
+      filler: ["Pink Baby's Breath", "Peach Wax Flower"],
+      branching: ["Apple Blossom"]
+    }],
+    amount: [{
+      prim: [3, 2],
+      sec: [4, 1],
+      fill: [1, 1],
+      branch: [2]
+    }]
+  },
+
+  {
+    name: "Blushing Whisper",
+    emotion: "peace",
+    flowerset: [{
+      primary: ["Blush Peony", "Pale Pink Ranunculus"],
+      secondary: ["Soft Pink Dahlia", "Pink Lisianthus"],
+      filler: ["Pink Baby's Breath"],
+      branching: ["White Sweet Pea"]
+    }],
+    amount: [{
+      prim: [3, 3],
+      sec: [2, 3],
+      fill: [2],
+      branch: [2]
+    }]
+  },
+
+  {
+    name: "Aurora Delight",
+    emotion: "peace",
+    flowerset: [{
+      primary: ["Lavender Dahlia", "Blush Peony"],
+      secondary: ["Lavender Hydrangea", "Lilac Lisianthus", "Blush Ranunculus"],
+      filler: ["Pink Wax Flower"],
+      branching: ["Lavender Sweet Pea"]
+    }],
+    amount: [{
+      prim: [3, 2],
+      sec: [1, 3, 2],
+      fill: [1],
+      branch: [2]
+    }]
+  },
+
+  {
+    name: "Royal Midnight",
+    emotion: "peace",
+    flowerset: [{
+      primary: ["Dusty Lavender Dahlia", "White Peony"],
+      secondary: ["Lavender Hydrangea", "White Ranunculus", "Lavender Lisianthus"],
+      filler: ["White Baby's Breath"],
+      branching: ["Lavender Sweet Pea"]
+    }],
+    amount: [{
+      prim: [3, 2],
+      sec: [1, 3, 3],
+      fill: [1],
+      branch: [2]
+    }]
+  },
+
+  {
+    name: "Golden Dawn",
+    emotion: "peace",
+    flowerset: [{
+      primary: ["Cream Ranunculus", "Blush Garden Rose"],
+      secondary: ["Peach Dahlia", "Blush Hydrangea"],
+      filler: ["White Baby's Breath"],
+      branching: ["Apple Blossom"]
+    }],
+    amount: [{
+      prim: [4, 2],
+      sec: [3, 1],
+      fill: [1],
+      branch: [2]
+    }]
+  },
+
+  {
+    name: "Everlasting Grace",
+    emotion: "peace",
+    flowerset: [{
+      primary: ["White Peony", "White Garden Rose"],
+      secondary: ["Blush Hydrangea", "White Lisianthus"],
+      filler: ["White Wax Flower"],
+      branching: ["White Sweet Pea"]
+    }],
+    amount: [{
+      prim: [3, 2],
+      sec: [1, 3],
+      fill: [2],
+      branch: [2]
+    }]
+  },
+
+  {
+    name: "Petal Poem",
+    emotion: "peace",
+    flowerset: [{
+      primary: ["Pink Peony", "Pink Ranunculus"],
+      secondary: ["Blush Dahlia", "Pink Lisianthus"],
+      filler: ["Pink Baby's Breath"],
+      branching: ["Apple Blossom"]
+    }],
+    amount: [{
+      prim: [3, 3],
+      sec: [2, 2],
+      fill: [2],
+      branch: [1]
+    }]
+  },
+
+  {
+    name: "Lavender Cloud",
+    emotion: "peace",
+    flowerset: [{
+      primary: ["Lavender Hydrangea", "Dusty Lavender Dahlia"],
+      secondary: ["White Ranunculus", "Lavender Lisianthus"],
+      filler: ["White Baby's Breath"],
+      branching: ["Lavender Sweet Pea"]
+    }],
+    amount: [{
+      prim: [1, 3],
+      sec: [3, 3],
+      fill: [1],
+      branch: [2]
+    }]
+  },
+
+  {
+    name: "Soft Remorse",
+    emotion: "apology",
+    flowerset: [{
+      primary: ["Blush Peony", "Peach Ranunculus"],
+      secondary: ["White Hydrangea", "Soft Pink Dahlia"],
+      filler: ["White Baby's Breath"],
+      branching: ["Blush Sweet Pea"]
+    }],
+    amount: [{
+      prim: [3, 3],
+      sec: [1, 2],
+      fill: [2],
+      branch: [2]
+    }]
+  },
+
+  {
+    name: "Gentle Clarity",
+    emotion: "apology",
+    flowerset: [{
+      primary: ["White Peony", "Blush Garden Rose"],
+      secondary: ["White Ranunculus", "Cream Lisianthus"],
+      filler: ["White Wax Flower"],
+      branching: ["White Sweet Pea"]
+    }],
+    amount: [{
+      prim: [3, 2],
+      sec: [3, 2],
+      fill: [1],
+      branch: [2]
+    }]
+  },
+
+  {
+    name: "Healing Whisper",
+    emotion: "apology",
+    flowerset: [{
+      primary: ["Peach Peony", "Blush Ranunculus"],
+      secondary: ["Blush Dahlia", "Peach Hydrangea"],
+      filler: ["Peach Baby's Breath"],
+      branching: ["Apple Blossom"]
+    }],
+    amount: [{
+      prim: [2, 3],
+      sec: [2, 1],
+      fill: [2],
+      branch: [1]
+    }]
+  },
+
+  {
+    name: "Mending Petals",
+    emotion: "apology",
+    flowerset: [{
+      primary: ["White Peony", "Soft Pink Ranunculus"],
+      secondary: ["White Dahlia", "Blush Garden Rose"],
+      filler: ["White Baby's Breath"],
+      branching: ["White Sweet Pea"]
+    }],
+    amount: [{
+      prim: [3, 3],
+      sec: [2, 2],
+      fill: [1],
+      branch: [2]
+    }]
+  },
+
+  {
+    name: "Humble Bloom",
+    emotion: "apology",
+    flowerset: [{
+      primary: ["Blush Peony", "White Garden Rose"],
+      secondary: ["Peach Ranunculus", "Soft Pink Lisianthus"],
+      filler: ["Pink Baby's Breath"],
+      branching: ["Apple Blossom"]
+    }],
+    amount: [{
+      prim: [2, 3],
+      sec: [2, 3],
+      fill: [1],
+      branch: [1]
+    }]
+  },
+
+  {
+    name: "Tender Resolve",
+    emotion: "apology",
+    flowerset: [{
+      primary: ["Peach Garden Rose", "Blush Dahlia"],
+      secondary: ["White Hydrangea", "Cream Ranunculus"],
+      filler: ["White Wax Flower"],
+      branching: ["Blush Sweet Pea"]
+    }],
+    amount: [{
+      prim: [3, 2],
+      sec: [1, 3],
+      fill: [1],
+      branch: [2]
+    }]
+  },
+
+  {
+    name: "Quiet Confession",
+    emotion: "apology",
+    flowerset: [{
+      primary: ["White Ranunculus", "Blush Peony"],
+      secondary: ["Soft Pink Dahlia", "White Lisianthus"],
+      filler: ["White Baby's Breath"],
+      branching: ["White Sweet Pea"]
+    }],
+    amount: [{
+      prim: [4, 2],
+      sec: [2, 3],
+      fill: [1],
+      branch: [2]
+    }]
+  },
+
+  {
+    name: "Peace Offering",
+    emotion: "apology",
+    flowerset: [{
+      primary: ["Blush Garden Rose", "Peach Peony"],
+      secondary: ["Blush Hydrangea", "Soft Pink Ranunculus"],
+      filler: ["Pink Baby's Breath"],
+      branching: ["Apple Blossom"]
+    }],
+    amount: [{
+      prim: [2, 3],
+      sec: [1, 3],
+      fill: [2],
+      branch: [1]
+    }]
+  },
+
+  {
+    name: "Sincere Solace",
+    emotion: "apology",
+    flowerset: [{
+      primary: ["White Peony", "Cream Ranunculus"],
+      secondary: ["White Dahlia", "Blush Lisianthus"],
+      filler: ["White Wax Flower"],
+      branching: ["White Sweet Pea"]
+    }],
+    amount: [{
+      prim: [3, 3],
+      sec: [2, 2],
+      fill: [1],
+      branch: [2]
+    }]
+  },
+
+  {
+    name: "Blush Amends",
+    emotion: "apology",
+    flowerset: [{
+      primary: ["Blush Peony", "Soft Pink Dahlia"],
+      secondary: ["Pink Ranunculus", "Blush Garden Rose"],
+      filler: ["Pink Baby's Breath"],
+      branching: ["Blush Sweet Pea"]
+    }],
+    amount: [{
+      prim: [3, 2],
+      sec: [2, 2],
+      fill: [1],
+      branch: [2]
+    }]
+  },
+
+  {
+    name: "Eternal Promise",
+    emotion: "love",
+    flowerset: [{
+      primary: ["Red Garden Rose", "Soft Pink Peony"],
+      secondary: ["Red Ranunculus", "Wine Dahlia"],
+      filler: ["White Baby's Breath"],
+      branching: ["Blush Sweet Pea"]
+    }],
+    amount: [{
+      prim: [4, 2],
+      sec: [3, 2],
+      fill: [2],
+      branch: [2]
+    }]
+  },
+
+  {
+    name: "Blushing Devotion",
+    emotion: "love",
+    flowerset: [{
+      primary: ["Blush Peony", "Pink Garden Rose"],
+      secondary: ["Pink Ranunculus", "Champagne Lisianthus"],
+      filler: ["Pink Baby's Breath"],
+      branching: ["Apple Blossom"]
+    }],
+    amount: [{
+      prim: [3, 3],
+      sec: [2, 2],
+      fill: [1],
+      branch: [1]
+    }]
+  },
+
+  {
+    name: "Crimson Confession",
+    emotion: "love",
+    flowerset: [{
+      primary: ["Red Rose", "Ruby Peony"],
+      secondary: ["Burgundy Dahlia", "Blush Ranunculus"],
+      filler: ["White Wax Flower"],
+      branching: ["Red Sweet Pea"]
+    }],
+    amount: [{
+      prim: [5, 2],
+      sec: [2, 2],
+      fill: [1],
+      branch: [2]
+    }]
+  },
+
+  {
+    name: "Lovers’ Harmony",
+    emotion: "love",
+    flowerset: [{
+      primary: ["Soft Pink Peony", "White Garden Rose"],
+      secondary: ["Pink Dahlia", "Champagne Ranunculus"],
+      filler: ["White Baby's Breath"],
+      branching: ["Blush Sweet Pea"]
+    }],
+    amount: [{
+      prim: [3, 2],
+      sec: [2, 2],
+      fill: [1],
+      branch: [2]
+    }]
+  },
+
+  {
+    name: "Endless Adoration",
+    emotion: "love",
+    flowerset: [{
+      primary: ["Red Garden Rose", "Blush Peony"],
+      secondary: ["White Hydrangea", "Pink Lisianthus"],
+      filler: ["Pink Baby's Breath"],
+      branching: ["Apple Blossom"]
+    }],
+    amount: [{
+      prim: [4, 2],
+      sec: [1, 2],
+      fill: [2],
+      branch: [1]
+    }]
+  },
+
+  {
+    name: "Velvet Passion",
+    emotion: "love",
+    flowerset: [{
+      primary: ["Wine Dahlia", "Red Peony"],
+      secondary: ["Burgundy Ranunculus", "Cream Lisianthus"],
+      filler: ["White Wax Flower"],
+      branching: ["Red Sweet Pea"]
+    }],
+    amount: [{
+      prim: [3, 3],
+      sec: [3, 2],
+      fill: [1],
+      branch: [2]
+    }]
+  },
+
+  {
+    name: "Serenade in Bloom",
+    emotion: "love",
+    flowerset: [{
+      primary: ["Pink Garden Rose", "Blush Peony"],
+      secondary: ["Soft Pink Ranunculus", "Pink Dahlia"],
+      filler: ["Pink Baby's Breath"],
+      branching: ["Blush Sweet Pea"]
+    }],
+    amount: [{
+      prim: [3, 2],
+      sec: [3, 2],
+      fill: [2],
+      branch: [2]
+    }]
+  },
+
+  {
+    name: "Adore You Always",
+    emotion: "love",
+    flowerset: [{
+      primary: ["Red Rose", "White Peony"],
+      secondary: ["Red Ranunculus", "Cream Dahlia"],
+      filler: ["White Baby's Breath"],
+      branching: ["White Sweet Pea"]
+    }],
+    amount: [{
+      prim: [4, 2],
+      sec: [3, 1],
+      fill: [1],
+      branch: [1]
+    }]
+  },
+
+  {
+    name: "Stolen Heart",
+    emotion: "love",
+    flowerset: [{
+      primary: ["Hot Pink Garden Rose", "Red Peony"],
+      secondary: ["Pink Dahlia", "Magenta Ranunculus"],
+      filler: ["Pink Baby's Breath"],
+      branching: ["Blush Sweet Pea"]
+    }],
+    amount: [{
+      prim: [3, 2],
+      sec: [2, 3],
+      fill: [1],
+      branch: [2]
+    }]
+  },
+
+  {
+    name: "Forever Yours",
+    emotion: "love",
+    flowerset: [{
+      primary: ["Champagne Rose", "Soft Pink Peony"],
+      secondary: ["Peach Ranunculus", "White Lisianthus"],
+      filler: ["White Baby's Breath"],
+      branching: ["Apple Blossom"]
+    }],
+    amount: [{
+      prim: [3, 3],
+      sec: [2, 2],
+      fill: [1],
+      branch: [1]
+    }]
+  },
+
+  {
+    name: "Moonlit Confession",
+    emotion: "love",
+    flowerset: [{
+      primary: ["Blush Garden Rose", "White Peony"],
+      secondary: ["Pink Ranunculus", "Cream Dahlia"],
+      filler: ["White Wax Flower"],
+      branching: ["Blush Sweet Pea"]
+    }],
+    amount: [{
+      prim: [2, 2],
+      sec: [3, 1],
+      fill: [1],
+      branch: [2]
+    }]
+  },
+
+  {
+    name: "Infinite Love",
+    emotion: "love",
+    flowerset: [{
+      primary: ["Red Garden Rose", "Champagne Peony"],
+      secondary: ["White Hydrangea", "Blush Lisianthus"],
+      filler: ["White Baby's Breath"],
+      branching: ["White Sweet Pea"]
+    }],
+    amount: [{
+      prim: [4, 2],
+      sec: [1, 2],
+      fill: [2],
+      branch: [2]
+    }]
+  }
+
+];
