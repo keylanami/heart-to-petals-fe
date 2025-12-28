bcc6a0efa5eb7924751323cf382de3315a088af8
klyroe <keylaanmrr@gmail.com>
2025-12-29 00:18:57 +0700

    fix adalah pokoknya

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
-  eslint: {
-    // Warning: Ini membolehkan deploy meski ada error ESLint
-    ignoreDuringBuilds: true,
-  },
   reactCompiler: true,
 };
 
