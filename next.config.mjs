17644e2aba854a1265bf8631fb6c66455ce1a2c3
klyroe <keylaanmrr@gmail.com>
2025-12-29 00:02:31 +0700

    ignore build errors

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
+  eslint: {
+    // Warning: Ini membolehkan deploy meski ada error ESLint
+    ignoreDuringBuilds: true,
+  },
   reactCompiler: true,
 };
 
